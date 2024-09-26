72b9d9e1047dc99369546b04a2a8f45349f05678
Paola Santanna <paola.santos@5by5.com>
2024-09-26 17:45:41 -0300

    Ajuste opção 3
    Ajuste na operação de opção 3

--- Sistema para Estacionamento DIO/Entities/Vehicle.cs
+++ Sistema para Estacionamento DIO/Entities/Vehicle.cs
@@ -34,50 +34,60 @@ public class Vehicle
         FinalPriceToPay = finalPriceToPay;
     }
 
+    public float RegisterHourPrice()
+    {
+        Console.Clear();
+        Console.WriteLine("\n>>>Welcome to the parking lot!<<<".ToUpper());
 
+        Console.WriteLine("Enter the price per hour (Use comma for decimals)");
+        float pricePerHour = float.Parse(Console.ReadLine());
 
-    //public float RegisterHourPrice()
-    //{
-    //    Console.Clear();
-    //    Console.WriteLine("\n>>>Welcome to the parking lot!<<<".ToUpper());
+        return pricePerHour;
+    }
 
-    //    Console.WriteLine("Enter the price per hour (Use comma for decimals)");
-    //    float pricePerHour = float.Parse(Console.ReadLine());
+    public void RegisterVehicle()
+    {
+        Console.WriteLine("\nEnter the following information:");
 
-    //    return pricePerHour;
-    //}
+        Console.WriteLine($"\nID: {this.ID}");
+        Console.Write("Vehicle Type: ");
+        this.VehicleType = Console.ReadLine();
 
-    //public void RegisterVehicle()
-    //{
-    //    Console.WriteLine("\nEnter the following information:");
+        Console.Write("Vehicle Model: ");
+        this.VehicleModel = Console.ReadLine();
 
-    //    Console.WriteLine($"\nID: {this.ID}");
-    //    Console.Write("Vehicle Type: ");
-    //    this.VehicleType = Console.ReadLine();
+        Console.Write("Vehicle year: ");
+        this.Year = int.Parse(Console.ReadLine());
 
-    //    Console.Write("Vehicle Model: ");
-    //    this.VehicleModel = Console.ReadLine();
+        Console.Write("Vehicle color: ");
+        this.Color = Console.ReadLine();
 
-    //    Console.Write("Vehicle year: ");
-    //    this.Year = int.Parse(Console.ReadLine());
+        Console.Write("Vehicle owner: ");
+        this.Owner = Console.ReadLine();
 
-    //    Console.Write("Vehicle color: ");
-    //    this.Color = Console.ReadLine();
+        Console.Write("Vehicle plate: ");
+        this.LicensePlate = Console.ReadLine();
+    }
 
-    //    Console.Write("Vehicle owner: ");
-    //    this.Owner = Console.ReadLine();
+    public void FinalPrice(float p)
+    {
+        Console.Write("\nTotal HOURS that the vehicle was parked: ");
+        this.TotalHorsParked = int.Parse(Console.ReadLine());
 
-    //    Console.Write("Vehicle plate: ");
-    //    this.LicensePlate = Console.ReadLine();
-    //}
+        this.FinalPriceToPay = p * this.TotalHorsParked;
 
-    //public void FinalPrice(float p)
-    //{
-    //    Console.Write("\nTotal HOURS that the vehicle was parked: ");
-    //    this.TotalHorsParked = int.Parse(Console.ReadLine());
+        Console.WriteLine($"\nPrice Per Hour: {p} \nFinal price: {this.FinalPriceToPay}");
+    }
 
-    //    this.FinalPriceToPay = p*this.TotalHorsParked;
-
-    //    Console.WriteLine($"\nPrice Per Hour: {p} \nFinal price: {this.FinalPriceToPay}");
-    //}
+    public override string? ToString()
+    {
+        return $"Vehicle ID: {this.ID} \nVehicle Type: {this.VehicleType} " +
+            $"\nVehicle Model: {this.VehicleModel} " +
+            $"\nYear: {this.Year} " +
+            $"\nColor: {this.Color} " +
+            $"\nOwner: {this.Owner} " +
+            $"\nVehicle plate: {this.LicensePlate} " +
+            $"\nTotal Hours Parked: {this.TotalHorsParked} " +
+            $"\nFinal price: {this.FinalPriceToPay}";
+    }
 }
